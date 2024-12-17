0c191667864f4e5bffdd89243615aed42b290e4a
Fernando Borquez <fernandoborquez215@gmail.com>
2024-12-17 00:54:11 -0700

    Added features and modules to plugins

--- init.lua
+++ init.lua
@@ -8,43 +8,36 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
+-- Leader key
+vim.g.mapleader = " "
 
+-- Lazy vim installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+    {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local plugins =
-{
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = 
-        {
-            transparent = true,
-        },
-    },
-
-    {    
-        "nvim-telescope/telescope.nvim", 
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    }
-}
-
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
+
+-- Telescope keymap
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', 'm', builtin.find_files, {})
+vim.keymap.set('n', '<C-m>', builtin.live_grep, {})
+
+-- Neotree keymap
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
 
 -- Color scheme
 vim.cmd("colorscheme tokyonight")
