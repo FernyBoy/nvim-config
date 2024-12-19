c82f5016f942be111912cd32fd247b79cb3e1d3a
Fernando Borquez <fernandoborquez215@gmail.com>
2024-12-19 04:49:58 -0700

    Languages added to treesitter

--- lua/files/plugins/treesitter.lua
+++ lua/files/plugins/treesitter.lua
@@ -1,4 +1,59 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate", 
+    build = ":TSUpdate",
+    config = function()
+        local conf = require("nvim-treesitter.configs")
+        conf.setup(
+        {
+            ensure_installed = 
+            {
+                "arduino",
+                "bash",
+                "c",
+                "c_sharp",
+                "cpp",
+                "css",
+                "csv",
+                "cuda",
+                "dockerfile",
+                "fish",
+                "fortran",
+                "fsharp",
+                "go",
+                "hjson",
+                "html",
+                "http",
+                "java",
+                "javascript",
+                "json",
+                "json5",
+                "latex",
+                "lua",
+                "luadoc",
+                "lua_patterns",
+                "perl",
+                "php",
+                "powershell",
+                "python",
+                "racket",
+                "regex",
+                "ruby",
+                "rust",
+                "scss",
+                "sql",
+                "ssh_config",
+                "tmux",
+                "toml",
+                "typescript",
+                "vim",
+                "vue",
+                "xml",
+                "yaml",
+            },
+
+            highlight = { enable = true },
+
+            indent = { enable = true },
+        })
+    end,
 }
