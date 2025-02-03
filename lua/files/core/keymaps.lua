48b5a5fa5705cc3254363e213af55b9bd65e1678
Fernando Borquez <fernandoborquez215@gmail.com>
2025-02-03 00:09:57 -0700

    Corrections

--- lua/files/core/keymaps.lua
+++ lua/files/core/keymaps.lua
@@ -7,7 +7,5 @@ vim.keymap.set('n', 'l', '<Down>', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', '<Up>', { noremap = true, silent = true })
 vim.keymap.set('n', ';', '<Right>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '1', '^', { noremap = true, silent = true })
-
 -- Neotree keymap
 vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
